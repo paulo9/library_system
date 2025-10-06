ecaa3aa06fcfc55d5dab6b54793b3986a4579a7d
Paulo César <paulo.silva@rdstation.com>
2025-10-06 12:54:28 -0300

    Update navigation links to redirect to the dashboard after sign-in and modify logout link to use a button for better UX.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    root_path
+    dashboard_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
